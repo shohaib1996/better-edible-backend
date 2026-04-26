53bbeece8613e4f4002cab4f90cfd65f96c6d4a4
Shohaib hossain <khanshohaibhossain@gmail.com>
2026-04-26 14:09:30 +0600

    added the fix

--- src/controllers/privateLabelProductController.ts
+++ src/controllers/privateLabelProductController.ts
@@ -33,7 +33,7 @@ export const getProductById = asyncHandler(async (req, res) => {
 });
 
 // ─────────────────────────────
-// Create Product
+// Create Products
 // ─────────────────────────────
 
 export const createProduct = asyncHandler(async (req, res) => {
